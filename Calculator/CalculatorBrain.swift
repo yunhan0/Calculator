1230262ef56bce08b7eaeebec2c7f1b149827af1
Yunhan <yunhan@4310D-124320-M.local>
2017-03-16 14:48:48 +1100

    initial commit

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,66 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Yunhan on 16/3/17.
+//  Copyright © 2017 Yunhan. All rights reserved.
+//
+
+import Foundation
+
+// Our model
+class CalculatorBrain {
+    private var accumulator = 0.0
+    
+    func setOperand(operand: Double) {
+        accumulator = operand
+    }
+    
+    var operations : Dictionary<String, Operation> = [
+        "π" : Operation.Constant(M_PI),
+        "e" : Operation.Constant(M_E),
+        "√" : Operation.UnaryOperation(sqrt),
+        "cos" : Operation.UnaryOperation(cos),
+        "×" : Operation.BinaryOperation({$0 * $1}),
+        "÷" : Operation.BinaryOperation({$0 / $1}),
+        "+" : Operation.BinaryOperation({$0 + $1}),
+        "-" : Operation.BinaryOperation({$0 - $1}),
+        "=" : Operation.Equals
+    ]
+    
+    enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
+    }
+    
+    
+    func performOperation(symbol: String) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .Constant(let value) : accumulator = value
+            case .BinaryOperation(let foo) : pending = PendingBinaryOperationInfo(binaryFunction: foo, firstOperand: accumulator)
+            case .UnaryOperation(let foo) : accumulator = foo(accumulator)
+            case .Equals :
+                if pending != nil {
+                    accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+                    pending = nil
+                }
+            }
+        }
+    }
+    
+    private var pending : PendingBinaryOperationInfo?
+    
+    struct PendingBinaryOperationInfo {
+        var binaryFunction: (Double, Double) -> Double
+        var firstOperand: Double
+    }
+    
+    var result: Double {
+        get {
+            return accumulator
+        }
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet private weak var display: UILabel!
+    
+    private var userIsInTheMiddleOfTyping: Bool = false
+    
+    @IBAction private func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentDisplay = display.text!
+            display.text = textCurrentDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
     }
+    
+    private var brain = CalculatorBrain()
 
-
+    @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(operand: displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        
+        if let mathmaticalSymbol = sender.currentTitle {
+            brain.performOperation(symbol: mathmaticalSymbol)
+        }
+        
+        displayValue = brain.result
+    }
+    
 }
 
